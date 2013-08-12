493cf69996793feaa3785f53b652025a465b425d
Misty De Meo <mistydemeo@gmail.com>
2013-08-12 08:46:30 -0700

    xnu-headers: fix Snow Leopard version
    Fixes #1.

--- xnu-headers.rb
+++ xnu-headers.rb
@@ -6,8 +6,7 @@ class XnuHeaders < Formula
   headers = {
     :tiger   => ['792.24.17', 'ac1c42ee5e3fcb1821e8c56706d63fb395989180'],
     :leopard => ['1228.15.4 ', 'd33e28f4db6973828e81f0f7ac366097eac80f9c'],
-    :snow_leopard => ['1504.15
-      .3', '7fe7b66f3db13a12137a33274f063e26facde45e'],
+    :snow_leopard => ['1504.15.3', '7fe7b66f3db13a12137a33274f063e26facde45e'],
     :lion => ['1699.32.7', 'da3df48952b40ad3b8612c7f639b8bf0f92fb414'],
     :mountain_lion => ['2050.22.13', 'a002806d1e64505c6a98c10af26186454818a9ff']
   }
